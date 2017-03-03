3845062462b9594dc61f993889278b14cec2abdf
Aleksander Kwiatkowski <bobikx@poczta.fm>
2017-03-03 10:32:38 +0100

    remove sample blog code from lib

--- data/src/blog.cr
+++
@@ -1,4 +0,0 @@
-require "./image_resizer"
-require "./data_manager"
-require "./post"
-require "./renderer"
--- data/src/image_resizer.cr
+++
@@ -1,8 +0,0 @@
-class Tremolite::ImageResizer
-  @@sizez = {
-    "medium" => {width: 780, height: 520, quality: 88},
-    "small" => {width: 600, height: 400, quality: 65},
-    "thumb" => {width: 60, height: 40, quality: 70},
-  }
-  @@quality = 70
-end
