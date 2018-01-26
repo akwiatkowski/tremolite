a55db10dacb90d01c7727a724ecb395ca03d4937
Aleksander Kwiatkowski <bobikx@poczta.fm>
2018-01-26 18:18:09 +0100

    change processed image output path (add month directory)

--- src/tremolite/image_resizer.cr
+++ src/tremolite/image_resizer.cr
@@ -5,10 +5,12 @@ class Tremolite::ImageResizer
   }
   @@quality = 70
 
+  PROCESSED_IMAGES_PATH = File.join(["images", "processed"])
+
   def initialize(@blog : Tremolite::Blog)
     @data_path = @blog.data_path.as(String)
     @public_path = @blog.public_path.as(String)
-    @processed_path = File.join([@public_path, "images", "processed"])
+    @processed_path = File.join([@public_path, PROCESSED_IMAGES_PATH])
     @logger = @blog.logger.as(Logger)
     @flags = "-strip -interlace Plane"
   end
@@ -24,13 +26,37 @@ class Tremolite::ImageResizer
     end
   end
 
+  # Use this method for all processed images paths
+  def self.processed_path_for_post(
+      processed_path : String,
+      post_year : Int32,
+      post_month : Int32,
+      post_slug : String,
+      prefix : String,
+      file_name : String
+    ) : String
+
+    post_month_string = post_month < 10 ? "0#{post_month}" : post_month.to_s
+    file_name_wo_jpg = file_name.gsub(/\.jpg/i, "")
+
+    return File.join([processed_path, post_year.to_s, post_month_string, "#{post_slug}_#{file_name_wo_jpg}_#{prefix}.jpg"])
+  end
+
   def resize_for_post(post : Tremolite::Post, overwrite : Bool, name = "header.jpg")
     img_url = File.join([@data_path, "images", post.year.to_s, post.slug, name])
     if File.exists?(img_url)
       # there are defined sizes of output images
       @@sizez.each do |prefix, resolution|
-        # TODO move it to method
-        output_url = File.join([@processed_path, "#{post.year}", "#{post.slug}_#{prefix}_#{name}"])
+        # output_url = File.join([@processed_path, "#{post.year}", "#{post.slug}_#{prefix}_#{name}"])
+        output_url = self.class.processed_path_for_post(
+            processed_path: @processed_path,
+            post_year: post.year,
+            post_month: post.time.month,
+            post_slug: post.slug,
+            prefix: prefix,
+            file_name: name
+          )
+
         resize_image(
           path: img_url,
           width: resolution[:width],
